424c7ea05662aa136940b895a555e12d2e700b29
Joseph Carnahan <joseph.carnahan@gmail.com>
2011-08-12 22:07:09 -0400

    Consolidated generic cards-related stuff into one file

--- src/main/scala/solitaire/Cards.scala
+++ src/main/scala/solitaire/Cards.scala
@@ -1,14 +1,21 @@
 package solitaire
 
+import scala.util.Random
+
 sealed case class Card (suit: Int, value: Int)
 
 object Card {
-
   def suits = 1 to 4
-
   def values = 1 to 13
 
   def allCards = for (suit <- suits; value <- values) yield new Card(suit, value)
 
 }
 
+sealed case class Deck(cards: Seq[Card]) {
+  def deal : (Card, Deck) = (cards.head, Deck(cards.tail))
+}
+
+object Deck {
+  def shuffle(rng: Random) = Deck(rng.shuffle(Card.allCards))
+}
