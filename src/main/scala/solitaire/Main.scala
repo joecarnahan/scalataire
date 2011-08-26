aa05ad339ae17b4f2be0abd764645a00435178e0
Joseph Carnahan <joseph.carnahan@gmail.com>
2011-08-25 23:03:15 -0400

    Modified main to run lots of trials

--- src/main/scala/solitaire/Main.scala
+++ src/main/scala/solitaire/Main.scala
@@ -4,12 +4,11 @@ object Main {
 
   def nl = System.getProperty("line.separator")
 
-  def main(args: Array[String]) = println(generateStats)
+  def main(args: Array[String]) = (1 to 1000).println(generateStats)
 
   def generateStats : String = {
     val wins = Game.playGame
     "Got " + wins.size + " wins" + nl + nl + wins.mkString("*****************************" + nl + nl)
-    //Game().getWin.map(_.mkString(nl)).getOrElse("No solution")
   }
 
 }
