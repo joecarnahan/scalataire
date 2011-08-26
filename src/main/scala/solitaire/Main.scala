94e2b7edef2778842d3a30fd59f7b279050ea4a7
Joseph Carnahan <joseph.carnahan@gmail.com>
2011-08-25 23:06:58 -0400

    Duh, compile before committing

--- src/main/scala/solitaire/Main.scala
+++ src/main/scala/solitaire/Main.scala
@@ -4,7 +4,7 @@ object Main {
 
   def nl = System.getProperty("line.separator")
 
-  def main(args: Array[String]) = (1 to 100).println(generateStats)
+  def main(args: Array[String]) = for (i <- 1 to 100) println(generateStats)
 
   def generateStats : String = {
     val wins = Game.playGame
