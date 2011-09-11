93be13a24ed17fe15500c5fae1061693af8af830
Joe Carnahan <joseph.carnahan@gmail.com>
2011-09-11 15:48:02 -0400

    Started to add comments, realized some classes need to be renamed

--- src/main/scala/solitaire/Cards.scala
+++ src/main/scala/solitaire/Cards.scala
@@ -1,7 +1,18 @@
+/**
+ * Code for representing playing cards, which could be used for any type of
+ * card game.
+ *
+ * @author Joe Carnahan
+ */
+
 package solitaire
 
-import scala.util.Random
-
+/**
+ * Playing cards are represented using integers to indicate the suit and the
+ * value.
+ *
+ * @todo Need to determine if using Ints is really faster than using enums would be
+ */
 sealed case class Card (suit: Int, value: Int) {
   def isBlack = suit % 2 == 0
   def isRed = !isBlack
@@ -15,15 +26,23 @@ object Card {
   def allCards = for (suit <- suits; value <- values) yield new Card(suit, value)
 }
 
+/**
+ * A deck is simply a list of cards that can be shuffled and from which the top
+ * card can be dealt.
+ */
 sealed case class Deck(cards: Seq[Card]) {
   def deal : (Card, Deck) = (cards.head, Deck(cards.tail))
 }
 
 object Deck {
-  val rng = new Random
+  val rng = new scala.util.Random
   def shuffle = Deck(rng.shuffle(Card.allCards))
 }
 
+/**
+ * A stack is a list of cards onto which cards can be placed and that can
+ * be flipped over.  A common use for a card stack would be a discard pile.
+ */
 sealed case class Stack(cards: List[Card]) {
   def top = if (cards.isEmpty) None else Some(cards.head)
   def put(card: Card) = Stack(card :: cards)
@@ -34,6 +53,7 @@ object Stack {
   def apply(): Stack = Stack(List[Card]())
 }
 
+// RESUME HERE
 sealed case class Pile(faceup: List[Card], facedown: List[Card]) {
 
   override def toString = faceup.toString + " on top of " + facedown.toString
